e7ecb4da7b695a5df8eceb554e057fdc8fc5a1c0
Samuel <tehsams@gmail.com>
2018-05-14 14:52:09 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/TpTomcatApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpTomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpTomcatApplication.class, args);
+	}
+}
